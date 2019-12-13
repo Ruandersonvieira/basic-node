791a6d77696d3fd96a674316e8b52a1ad316f9bc
Ruanderson Vieira <ruanderson@accalia.io>
2019-12-12 22:38:59 -0300

    :heavy_plus_sign: mongoose and models

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const MongoClient = require('mongodb');
 
 const app = express();
+
+const url = 'mongodb://localhost:27017/producthunter';
+
+MongoClient.connect(url, { useNewUrlParser : true });
+
+require('./src/models/Product');
 
 app.get('/', (req, res) => {
   return res.json({ message: "working" });
